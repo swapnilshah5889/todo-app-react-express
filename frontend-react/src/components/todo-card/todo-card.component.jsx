a8a7f77706813b730a6afd52ce6898d579948d1a
Swapnil Shah <swapnilshah5889@gmail.com>
2023-09-03 18:51:00 -0400

    Added checkbox in todo card to show completed or pending status

--- frontend-react/src/components/todo-card/todo-card.component.jsx
+++ frontend-react/src/components/todo-card/todo-card.component.jsx
@@ -2,17 +2,34 @@ import './todo-card.styles.css'
 import deleteImg from '../../assests/delete.svg'
 import editImg from '../../assests/edit.png'
 
-function TodoCard(props) {
 
-    const {todoData} = props;
+const CheckBox = ({isChecked=false, onCheckBoxChange}) => {
 
+    return (
+        <input 
+            checked={isChecked} 
+            onChange={onCheckBoxChange}
+            type="checkbox" 
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 
+            dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 
+            dark:border-gray-600">
+        </input>
+    );
+
+}
+
+function TodoCard({todoData, onDeleteClick, onEditClick}) {
 
     function handleDeleteTodo() {
-        props.onDeleteClick(todoData.id);
+        onDeleteClick(todoData.id);
     }
 
     function handleEditTodo() {
-        props.onEditClick(todoData);
+        onEditClick(todoData);
+    }
+
+    function onCheckBoxChange() {
+        // todoData.isDone = !todoData.isDone;
     }
 
     return (
@@ -39,7 +56,23 @@ function TodoCard(props) {
             </div>
 
             {/* Description */}
-            <p className='todo-description'>{todoData.description}</p>
+
+            <div className='grid grid-cols-12 gap-1'>
+                <div className='col-span-10'>
+                    <p className='todo-description'>{todoData.description}</p>
+                </div>
+
+                {/* Completed Status */}
+                <div className='flex col-span-2 justify-center items-center'>
+                    
+                    <CheckBox 
+                        isChecked={todoData.isDone}
+                        onCheckBoxChange={onCheckBoxChange}
+                    />
+                </div>
+            </div>
+
+            
 
         </div>
     );
