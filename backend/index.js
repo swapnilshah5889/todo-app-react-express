856f7d5c8e2c7b5297a540aa888e87a46899d69c
Swapnil Shah <swapnilshah5889@gmail.com>
2023-09-08 10:18:49 -0400

    Resolved Cors Error

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,10 @@ app.use(cors());
 
 // Cors routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 })
 
