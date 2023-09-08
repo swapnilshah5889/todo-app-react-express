3b4ee0264fabe7e3521b6b32780eb292d1118dc9
Swapnil Shah <swapnilshah5889@gmail.com>
2023-09-08 10:25:43 -0400

    Resolved Cors Error

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,14 @@ app.use(cors(
     }
 ));
 
-// Cors routes
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'todo-app-swapnilshah.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-})
+// // Cors routes
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'todo-app-swapnilshah.netlify.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// })
 
 
 // Sign up user
