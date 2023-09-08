d6c0d58cb295461a2903c67b09b0fa3ceaf0ba7a
Swapnil Shah <swapnilshah5889@gmail.com>
2023-09-08 10:22:38 -0400

    Resolved Cors Error

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,15 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://todo-app-swapnilshah.netlify.app'
+    }
+));
 
 // Cors routes
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'todo-app-swapnilshah.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
